e6d8ed32d13b7d65fbaec2e1554df9ed3952f4ef
abdurrahman <uzum.abdurrahman@outlook.com>
2022-08-08 23:00:07 +0300

    Created.

---
+++ ISE/firstproject/source1.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:49:48 08/08/2022 
+// Design Name: 
+// Module Name:    source1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module source1(
+    );
+
+
+endmodule
